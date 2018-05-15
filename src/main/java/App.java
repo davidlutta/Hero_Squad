5fbbdace19888e6fec7350811590fbd36d5c38f7
davidlutta <davidlutta2010@gmail.com>
2018-05-15 12:35:41 +0300

    Made Change to App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,7 +16,7 @@ public class App{
 
         ProcessBuilder process = new ProcessBuilder();
         Integer port;
-        if (process.environment().get("PORT") != null){
+        if (process.environment().get("PORT") != null) {
             port = Integer.parseInt(process.environment().get("PORT"));
         } else {
             port = 4567;
